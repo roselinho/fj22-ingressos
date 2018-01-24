1a37069496a46f081af007b2c733ad90c0523b1f
Mateus <mateus_twister@hotmail.com>
2018-01-24 20:43:00 -0200

    inserindo mudanças de mapping

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,6 +1,8 @@
 package br.com.caelum.ingresso.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,11 +11,14 @@ import br.com.caelum.ingresso.dao.SalaDao;
 
 @Controller
 public class SessaoController {
-
+	
+	@Autowired
 	private SalaDao salaDao;
 	
+	@Autowired
 	private FilmeDao filmeDao;
 	
+	@GetMapping("/admin/sessao")
 	public ModelAndView form(@RequestParam("salaId") Integer salaId) {
 		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
 		
